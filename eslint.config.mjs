02e0aed81982f0bb7d400e71e0d66f05d44e3d78
Fabricio Borgobello <fborgobello@gmail.com>
2025-03-14 21:40:48 +0100

    ESLint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import drizzle from "eslint-plugin-drizzle";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +15,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintConfigPrettier,
+  prettierPlugin, // Prettier should be last
+  {
+    plugins: { drizzle },
+    rules: {
+      ...drizzle.configs.recommended.rules,
+      "drizzle/enforce-delete-with-where": [
+        "error",
+        { drizzleObjectName: ["db"] },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
